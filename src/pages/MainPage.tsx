381a9e89e3da7b1161f07ca5c9c8fc08a834687f
har2525life <har2525life@outlook.com>
2023-07-04 17:30:39 +0900

    fix: created timer style

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -12,6 +12,10 @@ export default function MainPage() {
   console.log(history);
   const { pathname } = location;
 
+  const transitionPage = () => {
+    history.push("/timer");
+  };
+
   return (
     <>
       <Header title="Chain Timer" />
@@ -21,18 +25,38 @@ export default function MainPage() {
           <p className="">早速タイマーを作成しましょう</p>
         </div>
       ) : (
-        <div className="border-b">
-          <div className="flex flex-col justify-center h-20">
-            <div className="flex justify-between line p-10">
-              <p className="text-2xl">歯磨き</p>
-              <button className="h-10 w-10 bg-rose-200 rounded-full flex flex-col justify-center items-center text-xl">
-                <CaretForwardOutline
-                  color={"#fefefe"}
-                  height="30px"
-                  width="30px"
-                  style={{ marginLeft: "3px" }}
-                />
-              </button>
+        <div className="">
+          <div className="border-b">
+            <div className="flex flex-col justify-center h-20">
+              <div className="flex justify-between line px-8">
+                <p className="text-xl leading-10">歯磨き</p>
+                <button className="h-10 w-10 bg-rose-200 rounded-full flex flex-col justify-center items-center text-xl">
+                  <CaretForwardOutline
+                    onClick={transitionPage}
+                    color={"#fefefe"}
+                    height="30px"
+                    width="30px"
+                    style={{ marginLeft: "3px" }}
+                  />
+                </button>
+              </div>
+            </div>
+          </div>
+          
+          <div className="border-b">
+            <div className="flex flex-col justify-center h-20">
+              <div className="flex justify-between line px-8">
+                <p className="text-xl leading-10">歯磨き</p>
+                <button className="h-10 w-10 bg-rose-200 rounded-full flex flex-col justify-center items-center text-xl">
+                  <CaretForwardOutline
+                    onClick={transitionPage}
+                    color={"#fefefe"}
+                    height="30px"
+                    width="30px"
+                    style={{ marginLeft: "3px" }}
+                  />
+                </button>
+              </div>
             </div>
           </div>
         </div>
