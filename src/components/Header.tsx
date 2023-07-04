ea2dd4e1df9a8b624fef3b7407a5eb8e4ba1c33b
har2525life <har2525life@outlook.com>
2023-07-04 17:29:32 +0900

    fix: title vertical center

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 type HeaderTitle = {
-  title: string
-}
+  title: string;
+};
 
-export default function Header({title}: HeaderTitle) {
+export default function Header({ title }: HeaderTitle) {
   return (
     <header>
-      <nav className="bg-sky-400 border-gray-200 px-4 py-2.5">
-        <h1 className="text-center text-black text-2xl font-bold h-20">{title}</h1>
-      </nav>
+      <div className="flex flex-col justify-center items-center h-14 bg-sky-400 border-gray-200 px-4">
+        <h1 className=" text-black text-2xl font-bold ">{title}</h1>
+      </div>
     </header>
   );
 }
