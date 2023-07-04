6c01dd0b9b9f3fb97a2566741d00bc0b2aebfafa
har2525life <har2525life@outlook.com>
2023-07-04 16:23:23 +0900

    add: setting page

--- src/pages/SettingPage.tsx
+++ src/pages/SettingPage.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import Header from "../components/Header";
 
 export default function SettingPage() {
-  return <div>SettingPage</div>;
+  return (
+    <div className="">
+      <Header title="Setting" />
+      <div className="mx-8">
+        <div className="w-full h-16 bg-gray-200"></div>
+        <div className="w-full h-16 bg-gray-200 mt-8"></div>
+        <div className="w-full h-16 bg-gray-200"></div>
+        <div className="w-full h-16 bg-gray-200 mt-8"></div>
+      </div>
+    </div>
+  );
 }
