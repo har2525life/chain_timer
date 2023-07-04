b54d89f4d41d6106dead3d48593042ef7024a451
har2525life <har2525life@outlook.com>
2023-07-04 17:30:09 +0900

    add: log page base

--- src/pages/LogPage.tsx
+++ src/pages/LogPage.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Header from '../components/Header'
+import CreateButton from '../components/CreateButton'
 
 export default function LogPage() {
   return (
-    <div>LogPage</div>
+    <div className="">
+      <Header title="Log" />
+      <div className="mx-8"></div>
+      <CreateButton page="" />
+    </div>
   )
 }
