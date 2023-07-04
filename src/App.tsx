1324bcd854ffdeaabd47ac66b8a7be5d5cbafa1b
har2525life <har2525life@outlook.com>
2023-07-04 11:05:15 +0900

    fix: nit file delete

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,7 @@ import {
 } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
 import { ellipse, square, triangle } from "ionicons/icons";
-import Tab1 from "./pages/Tab1";
-import Tab2 from "./pages/Tab2";
-import Tab3 from "./pages/Tab3";
+import MainPage from "./pages/MainPage";
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
@@ -42,17 +40,17 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
+          <Route exact path="/main">
+            <MainPage />
           </Route>
-          <Route exact path="/tab2">
+          {/* <Route exact path="/tab2">
             <Tab2 />
           </Route>
           <Route path="/tab3">
             <Tab3 />
-          </Route>
+            </Route> */}
           <Route exact path="/">
-            <Redirect to="/tab1" />
+            <Redirect to="/main" />
           </Route>
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
