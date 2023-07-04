26e8c6acf3db8bd1eab3715b8bf6b0d9a643f1ca
har2525life <har2525life@outlook.com>
2023-07-04 17:29:56 +0900

    fix: timer style

--- src/pages/CreateTimerPage.tsx
+++ src/pages/CreateTimerPage.tsx
@@ -9,23 +9,25 @@ export default function CreateTimerPage() {
   return (
     <>
       <Header title="Create Timer" />
-      <div className="mx-10">
+      <div className="mx-8 mt-4">
+        <label htmlFor="timerName">タイマーの名前</label>
         <input
+          id="timerName"
           type="text"
-          placeholder="タイマーの名前"
-          className="w-full border-b px-2 h-20 text-xl"
+          placeholder="morning routing"
+          className="w-full border px-2 h-16 text-xl"
         />
-        <div className="mt-14">
-          <div className="border w-hull h-16 border-l-2 border-indigo-600 flex justify-between items-center px-2">
+        <div className="mt-8">
+          <div className="border w-hull h-16 border-l-4 border-sky-600 flex justify-between items-center px-2">
             <div className="flex">
-              <div className="w-8 h-8 rounded-full bg-sky-200"></div>
+              <div className="w-6 h-6 rounded-full border-2 border-sky-200"></div>
               <h1 className="text-xl ml-3">インターバルタイマー</h1>
             </div>
             <p>00m00s</p>
           </div>
-          <div className="border w-hull h-16 border-l-2 border-indigo-600 flex justify-between items-center px-2 mt-5">
+          <div className="border w-hull h-16 border-l-4 border-sky-600 flex justify-between items-center px-2 mt-4">
             <div className="flex">
-              <div className="w-8 h-8 rounded-full bg-sky-200"></div>
+              <div className="w-6 h-6 rounded-full border-2 border-sky-200"></div>
               <h1 className="text-xl ml-3">インターバルタイマー</h1>
             </div>
             <p>00m00s</p>
