c55f10d8fa07f1529010232f9a8b7fcf99810314
har2525life <har2525life@outlook.com>
2023-07-04 11:21:09 +0900

    add: header components base

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function Header() {
+  return (
+    <header>
+      <nav className="bg-sky-400 border-gray-200 px-4 py-2.5">
+        <h1 className="text-center text-black text-2xl font-bold h-20">Chain Timer</h1>
+      </nav>
+    </header>
+  );
+}
