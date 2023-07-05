d7108791f88a13a225f192a4d854256c6eee629d
har2525life <har2525life@outlook.com>
2023-07-05 10:30:23 +0900

    add: setting page wording

--- src/pages/SettingPage.tsx
+++ src/pages/SettingPage.tsx
@@ -6,10 +6,13 @@ export default function SettingPage() {
     <div className="">
       <Header title="Setting" />
       <div className="mx-8">
-        <div className="w-full h-16 bg-gray-200"></div>
-        <div className="w-full h-16 bg-gray-200 mt-8"></div>
-        <div className="w-full h-16 bg-gray-200"></div>
-        <div className="w-full h-16 bg-gray-200 mt-8"></div>
+        <div className="w-full leading-10 px-3 h-10 bg-gray-200 flex justify-between">
+            <p>バージョン</p>
+            <p>1.0.0</p>
+        </div>
+        <div className="w-full leading-10 px-3 h-10 bg-gray-200 flex flex-col align-center mt-8">ご意見Box</div>
+        <div className="w-full leading-10 px-3 h-10 bg-gray-200 flex align-center mt-1">ヘルプ</div>
+        <div className="w-full leading-10 px-3 h-10 bg-gray-200 flex align-center mt-8">バックアップ機能を有効にする</div>
       </div>
     </div>
   );
