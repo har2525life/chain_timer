47313524b969d3f8e9b93fdb0d356bc701d9a766
har2525life <har2525life@outlook.com>
2023-07-06 11:34:11 +0900

    fix: timer count up logical

--- src/pages/TimerPage.tsx
+++ src/pages/TimerPage.tsx
@@ -1,25 +1,61 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Header from "../components/Header";
+import { IonContent, IonPage } from "@ionic/react";
 
 const TimerPage = () => {
+  const [isStart, setIsStart] = useState(true);
+  const [count, setCount] = useState(0);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  const startTimer = () => {
+    if (intervalRef.current !== null) return;
+    intervalRef.current = setInterval(() => {
+      setCount((c) => c + 1);
+    }, 1000);
+    setIsStart(true);
+  };
+
+  useEffect(() => {
+    startTimer();
+  }, []);
+  const stopTimer = () => {
+    if (intervalRef.current === null) return;
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setIsStart(false);
+  };
   return (
-    <div className="">
-      <Header title="Fight" />
-      <div className="mx-8">
-        <div className="flex flex-col items-center justify-center">
-          <h1>timer title</h1>
-          <div className="flex">
-            <p>00</p>
-            <p>m</p>
-            <p>:</p>
-            <p>00</p>
-            <p>s</p>
+    <IonPage className="">
+      <IonContent>
+        <Header title="Fight" />
+        <div className="mx-8">
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="text-2xl">timer title</h1>
+            <div className="flex my-4">
+              <p>00</p>
+              <p>m</p>
+              <p>:</p>
+              <p>{count}</p>
+              <p>s</p>
+            </div>
           </div>
+          {isStart ? (
+            <button
+              onClick={stopTimer}
+              className="w-full h-10 bg-rose-200 rounded"
+            >
+              STOP
+            </button>
+          ) : (
+            <button
+              onClick={startTimer}
+              className="w-full h-10 bg-sky-200 rounded"
+            >
+              PLAY
+            </button>
+          )}
         </div>
-        <button className="w-full h-10 bg-sky-200 rounded">PAUSE</button>
-        <button className="w-full h-10 bg-rose-200 rounded">STOP</button>
-      </div>
-    </div>
+      </IonContent>
+    </IonPage>
   );
 };
 
